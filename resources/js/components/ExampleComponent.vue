c3aa71170d5e531a7684d25fa3893f377c04640f
air2021 <>
2021-09-24 16:36:27 +0900

    start

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="container">
+        <!-- <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+        <br />
+        <div class="alert alert-primary" role="alert">
+            Mitumori System Mockup
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                「点検種別」を選ぶ
+                </div>
+                <div class="col">
+                        <form>
+                            <select id="tenken" v-model="tenken">
+                                <option>C点検</option>
+                                <option>F点検</option>
+                                <option>負荷試験</option>
+                                <option>修理</option>
+                            </select>
+                        </form>
+                </div>
+            </div>
+            <br />
+            <div class="row">
+                <div class="col-3">
+                「機種」を選ぶ
+                </div>
+                <div class="col">
+                        <form>
+                            <select id="os" v-model="type">
+                                <option>AP10</option>
+                                <option>AP20</option>
+                                <option>AY500</option>
+                                <option>GT1000</option>
+                            </select>
+                        </form>
+                </div>
+            </div>
+            <br />
+            <div class="row">
+                <div class="col-3">
+                    「始動方式」を選ぶ
+                </div>
+                <div class="col">
+                    <form>
+                        <label for="battery">電気始動</label>
+                        <input type="radio" id="battery" value="電気始動" v-model="starting"/>
+                        <label for="air">空気始動</label>
+                        <input type="radio" id="air" value="空気始動" v-model="starting" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return{
+                tenken:'',
+                type:'',
+                starting:''
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/test.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel vue test</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
