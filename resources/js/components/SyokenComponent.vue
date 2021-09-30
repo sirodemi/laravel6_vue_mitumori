253913b95b103b8a067a00c57b69ad286d119113
air2021 <>
2021-10-01 07:03:17 +0900

    select value OK

--- resources/js/components/SyokenComponent.vue
+++ resources/js/components/SyokenComponent.vue
@@ -8,18 +8,33 @@
         <div class="container">
             <div class="row">
                 <div class="col-3">
-                「症状」
+                    「症状 1st」ユーザーレベル
                 </div>
                 <div class="col">
                     <form>
-                        <select id="battery" v-model="battery" v-on:change="onchange">
-                            <option>バッテリー経年劣化</option>
-                            <option>バッテリー液不足</option>
+                        <select id="userLevel" v-model="userLevel" v-on:change="onchange_1stLevel">
+                            <option value = "1">発電機が動かない</option>
+                            <option value = "2">水が漏れている</option>
                         </select>
                     </form>
                 </div>
             </div>
+
             <br />
+            <div class="row">
+                <div class="col-3">
+                    「症状 2nd」推定レベル
+                </div>
+                <div class="col">
+                    <select v-model="selected">
+                        <option v-for="(option,key) in options" :key="key" v-bind:value="option.value">
+                            {{ option.text }}
+                        </option>
+                    </select>
+                </div>
+            </div>
+
+
 
             <div class="row">
                 <div class="col-3">
@@ -47,7 +62,6 @@
                         </option>
                     </select>
                 </div>
-
             </div>
 
             <br />
@@ -68,6 +82,7 @@
     export default {
         data:function(){
             return{
+                userLevel:'',
                 battery:'',
                 selected:'',
                 options:'',
@@ -75,8 +90,22 @@
             }
         },
         methods:{
+            onchange_1stLevel:function(){
+                console.log('change93',this.userLevel);
+                if(this.userLevel == '1'){
+                    this.options=[
+                        { text: 'バッテリー交換　推奨', value: '経年劣化の為、バッテリー交換を推奨します' },
+                        { text: 'バッテリー交換　実施', value: '経年劣化の為、バッテリー交換を実施しました' },
+                    ]
+                }else{
+                    this.options=[
+                        { text: 'バッテリー液補充　推奨', value: 'バッテリー液の補充を推奨します' },
+                        { text: 'バッテリー液補充　実施', value: 'バッテリー液の補充を実施しました' },
+                    ]
+                }
+            },
             onchange:function(){
-                console.log('change70',this.battery);
+                console.log('change95',this.battery);
                 if(this.battery == 'バッテリー経年劣化'){
                     this.options=[
                         { text: 'バッテリー交換　推奨', value: '経年劣化の為、バッテリー交換を推奨します' },
